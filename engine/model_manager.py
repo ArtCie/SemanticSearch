9d5f7e32c70fb5b64f62f37c8c64146949cbaa76
aciesielczyk <aciesielczyk@novelaneuro.com>
2022-12-21 18:41:50 +0100

    ui init

--- engine/model_manager.py
+++ engine/model_manager.py
@@ -30,6 +30,6 @@ class ModelManager:
         text = self.transform_text(text.lower())
         distances = get_distance(distance, self._model, text)
         responses = []
-        for index in self.get_n_min_indexes(distances, 10):
+        for index in self.get_n_min_indexes(distances, 16):
             responses.append(f'{distances[index]} - {self._corpus[index]}')
         return responses
